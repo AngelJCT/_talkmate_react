7504d085182c4f4dff7e1a101f5d56a6d3170dbe
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-09-28 12:00:40 -0400

    adding glassmorphism to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,20 +17,20 @@ function Sidebar({ isOpen, onStateChange, setChatLog, conversations, activeConve
   return (
     <Menu
       isOpen={isOpen} onStateChange={onStateChange}
-      className='bg-gray-300'
+      className='bg-gray-50 backdrop-blur bg-opacity-[.55] bg-clip-padding rounded-br-lg rounded-tr-lg'
       styles={{ bmMenu: { zIndex: 5 } }}
     >
-      <button onClick={startNewChat} className='bg-custom-color font-medium text-xl w-full pt-5 pb-5 shadow-lg text-white'>
+      <button onClick={startNewChat} className='bg-custom-color font-medium text-xl w-full pt-5 pb-5 shadow-lg text-white rounded-tr-lg'>
         New Chat
       </button>
       <h2 className='font-bold pt-5 text-xl text-center mb-4 text-custom-blue pr-2 shadow-xl pb-5 rounded-b-xl'>Conversations</h2>
       {conversations.map((conversation, index) => (
         <div
           key={index}
-          className='mb-1'
+          className='m-2'
           onClick={() => loadConversation(index)}
         >
-          <h3 className={`font-bold text-gray-600 pl-3 cursor-pointer border-b-2 border-gray-300 hover:bg-gray-400 pb-2 pt-2 ${index === activeConversationIndex ? 'text-blue-700' : ''}`}>{conversation.title}</h3>
+          <h3 className={`font-medium text-custom-blue pl-3 cursor-pointer border-4 border-custom-blue border-opacity-20 hover:bg-gray-300 rounded-lg pb-2 pt-2 ${index === activeConversationIndex ? 'text-blue-700' : ''}`}>{conversation.title}</h3>
         </div>
       ))}
     </Menu>
