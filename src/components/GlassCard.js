1b41c9757e9b6724b1bd1f5089aa5cde32ef0d73
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-09-29 13:25:46 -0400

    making glasscards slide on small devices and increasing contrast between text and background

--- src/components/GlassCard.js
+++ src/components/GlassCard.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 function GlassCard({ hide }) {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    const Card = ({title, content}) => (
+        <div className="backdrop-blur-md bg-gray-50 bg-opacity-30 bg-clip-padding border border-white border-opacity-20 p-4 rounded-xl shadow-xl w-full h-32 transition-transform duration-700 ease-in-out">
+            <h2 className="text-xl mb-2 text-custom-blue font-bold">{title}</h2>
+            <p className="text-gray-100 font-medium">{content}</p>
+        </div>
+      );
+
   return (
     <div className={`flex flex-col justify-end xs:mt-4 md:mt-0 ${hide ? 'hide' : ''}`}>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 place-items-center mx-4 md:mx-0'>
-            <div className="backdrop-blur-md bg-gray-50 bg-opacity-30 bg-clip-padding border border-white border-opacity-20 p-4 rounded-xl shadow-xl w-full h-32 transition-transform duration-700 ease-in-out">
-                <h2 className="text-xl mb-2 text-custom-blue font-medium">How can I tell someone</h2>
-                <p className="text-gray-100">that I'm grateful for them in Japanese?</p>
-            </div>
-            <div className="backdrop-blur-md bg-gray-50 bg-opacity-30 bg-clip-padding border border-white border-opacity-20 p-4 rounded-xl shadow-xl w-full h-32 transition-transform duration-700 ease-in-out">
-                <h2 className="text-xl mb-2 text-custom-blue font-medium">How can I introduce myself</h2>
-                <p className="text-gray-100">in Japanese in different scenarios?</p>
-            </div>
-            <div className="backdrop-blur-md bg-gray-50 bg-opacity-30 bg-clip-padding border border-white border-opacity-20 p-4 rounded-xl shadow-xl w-full h-32 transition-transform duration-700 ease-in-out xs:hidden sm:hidden md:hidden lg:block">
-                <h2 className="text-xl mb-2 text-custom-blue font-medium">What is the difference between</h2>
-                <p className="text-gray-100">"Ore wa" and "Watashi wa" when referring to myself?</p>
-            </div>
-            <div className="backdrop-blur-md bg-gray-50 bg-opacity-30 bg-clip-padding border border-white border-opacity-20 p-4 rounded-xl shadow-xl w-full h-32 transition-transform duration-700 ease-in-out xs:hidden sm:hidden md:hidden lg:block">
-                <h2 className="text-xl mb-2 text-custom-blue font-medium">Elaborate more about how to refers to someone</h2>
-                <p className="text-gray-100">in respectfully and in casual occasions.</p>
-            </div>
+        <div className='md:hidden mx-5'>
+            <Slider arrows={false} {...settings}>
+                <Card title="How can I tell someone" content="that I'm grateful for them in Japanese?" />
+                <Card title="How can I introduce myself" content="in Japanese in different scenarios?" />
+                <Card title="What is the difference between" content='"Ore wa" and "Watashi wa" when referring to myself?' />
+                <Card title="Elaborate more about how to refers to someone" content="in respectfully and in casual occasions." />
+            </Slider>
+        </div>
+        <div className='hidden md:grid grid-cols-2 gap-6 place-items-center mx-4 md:mx-0'>
+            <Card title="How can I tell someone" content="that I'm grateful for them in Japanese?" />
+            <Card title="How can I introduce myself" content="in Japanese in different scenarios?" />
+            <Card title="What is the difference between" content='"Ore wa" and "Watashi wa" when referring to myself?' />
+            <Card title="Elaborate more about how to refers to someone" content="in respectfully and in casual occasions." />
         </div>
     </div>
   );
