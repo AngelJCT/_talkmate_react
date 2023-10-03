be3e94e1c0167d1270d379b733f5ee881b01e410
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-10-03 16:48:26 -0400

    changing typing animation and style

--- src/components/TypingAnimation.jsx
+++ src/components/TypingAnimation.jsx
@@ -1,11 +1,19 @@
+import { LeapFrog } from "@uiball/loaders";
+
 const TypingAnimation = () => {
     return (
-        <div className="flex items-center space-x-2">
+        <LeapFrog
+            size={50}
+            speed={2.5}
+            color="#FFF5F7FA"
+        />
+    )
+}
+
+{/* <div className="flex items-center space-x-2">
             <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse"></div>
             <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-75"></div>
             <div className="w-4 h-4 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 animate-pulse delay-150"></div>
-        </div>
-    )
-}
+        </div> */}
 
 export default TypingAnimation
