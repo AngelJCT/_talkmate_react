4270ce7932bf2717b034d3398381b861d41a5299
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-11-06 18:32:15 -0400

    updating api call to gpt-4-turbo and fixing prompt

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,34 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const handleSubmit = (event, setInputValue, inputValue, setChatLog, sendMessage, setIsLoading, chatLog) => {
-    event.preventDefault();
+export const handleSubmit = (
+  event,
+  setInputValue,
+  inputValue,
+  setChatLog,
+  sendMessage,
+  setIsLoading,
+  chatLog
+) => {
+  event.preventDefault();
 
-    setChatLog((prevChatLog) => [...prevChatLog, { type: 'user', message: inputValue }]);
+  setChatLog((prevChatLog) => [
+    ...prevChatLog,
+    { type: "user", message: inputValue },
+  ]);
 
-    sendMessage(inputValue, setChatLog, setIsLoading, chatLog);
+  sendMessage(inputValue, setChatLog, setIsLoading, chatLog);
 
-    setInputValue('');
-}
+  setInputValue("");
+};
 
-export const sendMessage = async (message, setChatLog, setIsLoading, chatLog) => {
-    const url = "https://api.openai.com/v1/chat/completions";
-    const headers = {
-        'Content-type': 'application/json',
-        'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}` 
-    };
+export const sendMessage = async (
+  message,
+  setChatLog,
+  setIsLoading,
+  chatLog
+) => {
+  const url = "https://api.openai.com/v1/chat/completions";
+  const headers = {
+    "Content-type": "application/json",
+    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+  };
 
-    const data = {
-        model: "gpt-3.5-turbo",
-        messages: [
-            { 
-                "role": "system",
-                "content": `You are a Japanese native speaker named Satoru.
+  const data = {
+    model: "gpt-4-1106-preview",
+    messages: [
+      {
+        role: "system",
+        content: `You are a Japanese native speaker named Satoru.
                             Your job is to help me learn Japanese by always providing translation, pronunciation and examples.
                             Your knowledge in Japanese language is both wide and deep. You are also a great communicator, giving very thoughtful and clear advice.
                             Always give tips of what to do and what not to do according to Japan culture. For any reason,
                             do not answer anything outside of your task (teach Japanese language), and always go the extra mile.
                             Always provide your response in this format:
-                            $answer
 
                             Translation:
                             $translation
@@ -43,54 +58,56 @@ export const sendMessage = async (message, setChatLog, setIsLoading, chatLog) =>
                             $cultural_tips
 
                             Notes:
-                            $notes`
-                            
-            },
-            ...chatLog.map(msg => ({ role: msg.type === 'user' ? 'user' : 'assistant', content: msg.message })),  // map existing chat log to API format
-            {
-                "role": "user",
-                "content": message
-            }
-        ]
-    };
+                            $notes`,
+      },
+      ...chatLog.map((msg) => ({
+        role: msg.type === "user" ? "user" : "assistant",
+        content: msg.message,
+      })), // map existing chat log to API format
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+  };
 
-    setIsLoading(true);
+  setIsLoading(true);
 
-    try {    
-        const response = await axios.post(url, data, { headers: headers });
-        const newMessage = {
-            type: 'bot',
-            message: response.data.choices[0].message.content,
-            timestamp: new Date().toISOString()
-        };
-        setChatLog((prevChatLog) => [...prevChatLog, newMessage]);
-        setIsLoading(false);
-        localStorage.setItem('chatLog', JSON.stringify([...chatLog, newMessage]))
-    } catch (error) {
-        setIsLoading(false);
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.error('Response data:', error.response.data);
-            console.error('Response status:', error.response.status);
-            console.error('Response headers:', error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.error('Request data:', error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.error('Error message:', error.message);
-        }
+  try {
+    const response = await axios.post(url, data, { headers: headers });
+    const newMessage = {
+      type: "bot",
+      message: response.data.choices[0].message.content,
+      timestamp: new Date().toISOString(),
     };
-}
+    setChatLog((prevChatLog) => [...prevChatLog, newMessage]);
+    setIsLoading(false);
+    localStorage.setItem("chatLog", JSON.stringify([...chatLog, newMessage]));
+  } catch (error) {
+    setIsLoading(false);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("Request data:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error message:", error.message);
+    }
+  }
+};
 
 export const clearChat = (setChatLog) => {
-    if (window.confirm("Are you sure you want to clear the chat?")) {
-        setChatLog([]);
-        localStorage.removeItem('chatLog');
-    }
-}
+  if (window.confirm("Are you sure you want to clear the chat?")) {
+    setChatLog([]);
+    localStorage.removeItem("chatLog");
+  }
+};
 
 export const saveConversation = (setIsDialogOpen) => {
-    setIsDialogOpen(true)
-}
+  setIsDialogOpen(true);
+};
