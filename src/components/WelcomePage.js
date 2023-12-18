55494abc4f59c07475d74677f5ae7b38b4504d3e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-12-18 12:03:09 -0400

    changing gradient sizes and blur, fixing text

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -32,13 +32,10 @@ const WelcomePage = () => {
   return (
     <div className="welcome-page">
       <div className="flex flex-col">
-        <div className="gradient-01 z-0 absolute"></div>
-        <div className="gradient-02 z-0 absolute"></div>
-        <div className="flex flex-col items-center justify-center h-screen bg-[#142f4df7] min-h-screen overflow-y-auto">
-          <h1 className="header-text font-bold xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center mx-5 mb-8 text-gray-100 z-20">
-            Welcome to TalkMate!
-          </h1>
+        <div className="gradient-01 z-0 absolute md:w-[550px] sm:w-[400px] sm:h-[350px] sm:blur-[190px] lg:w-[800px] md:h-[350px] lg:h-[550px] md:blur-[200px] lg:blur-[270px] xs:blur-[120px]"></div>
+        <div className="gradient-02 z-0 absolute md:w-[550px] sm:w-[400px] sm:h-[350px] sm:blur-[200px] lg:w-[700px] md:h-[350px] lg:h-[550px] md:blur-[250px] lg:blur-[300px] xs:blur-[130px]"></div>
 
+        <div className="flex flex-col items-center justify-center h-screen bg-[#142f4df7] min-h-screen overflow-y-auto">
           <div className="circular-container mb-8 shadow-2xl xs:w-[150px] xs:h-[150px] sm:w-[200px] sm:h-[200px] mx-auto">
             <img
               src="/assets/images/result.jpg"
@@ -46,15 +43,16 @@ const WelcomePage = () => {
               className="w-full"
             />
           </div>
+          <h1 className="header-text font-bold xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl xs:mx-3 sm:mx-5 md:mx-10 lg:mx-14 mb-8 text-gray-100 z-20">
+            Welcome to TalkMate!
+          </h1>
 
           <div className="text-container text-center px-4 xs:mx-3 sm:mx-5 md:mx-10 lg:mx-14 max-w-4xl align-middle mb-8">
             <p className="font-medium xs:text-sm sm:text-xl md:text-2xl text-gray-100">
-              Your new AI language learning assistant chatbot.
-              <br />
-              TalkMate is your companion in mastering new languages. It provides
-              translation, pronunciation, and cultural insights to help you not
-              only learn the language but also understand the people and their
-              culture.
+              Your new AI language learning assistant chatbot. TalkMate is your
+              companion in mastering new languages. It provides translation,
+              pronunciation, and cultural insights to help you not only learn
+              the language but also understand the people and their culture.
               <br />
               <br />
               Ready to embark on your language learning journey?
